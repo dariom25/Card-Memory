23feae949ad04b88cefcf7430522f0d2d656625e
dariom25 <dario@maciey.de>
2024-03-20 15:34:51 +0100

    Use prettier

--- src/Card/card.jsx
+++ src/Card/card.jsx
@@ -1,15 +1,14 @@
-import "./card.css"
+import "./card.css";
 
-function Card({name, imageLink, soundHandler}) {
-
-    return(
-        <div className="card-container" onClick={soundHandler}>
-            <div className="picture-container">
-                <img src={imageLink} alt={name} />
-            </div>
-            <h3>{name}</h3>
-        </div>
-    )
+function Card({ name, imageLink, soundHandler }) {
+  return (
+    <div className="card-container" onClick={soundHandler}>
+      <div className="picture-container">
+        <img src={imageLink} alt={name} />
+      </div>
+      <h3>{name}</h3>
+    </div>
+  );
 }
 
-export {Card}
+export { Card };
