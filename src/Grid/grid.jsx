63ca6572f66c946a24fff0d1fd1faca6c1611490
dariom25 <dario@maciey.de>
2024-03-25 20:54:06 +0100

    Fetch pokemondata correctly

--- src/Grid/grid.jsx
+++ src/Grid/grid.jsx
@@ -1,18 +1,55 @@
 import "./grid.css";
 import { Card } from "../Card/card.jsx";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Grid() {
-  useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/pikachu", { mode: "cors" }).then(
-      function (response) {
-        console.log(response.json());
-      },
-    ),
-      [];
+  const [pokemonInfos, setPokemonInfos] = useState([]);
+  const pokemons = pokemonInfos.map((pokemon) => {
+    return (
+      <Card
+        key={pokemon.id}
+        name={pokemon.name}
+        imageLink={pokemon.front_default}
+      />
+    );
   });
 
-  return <Card name={"Pikachu"} />;
+  useEffect(() => {
+    const pokemonNames = [
+      "duskull",
+      "banette",
+      "misdreavus",
+      "gengar",
+      "phantump",
+      "lampent",
+      "shiinotic",
+      "mismagius",
+      "drifloon",
+      "sableye",
+      "spiritomb",
+      "spectrier",
+    ];
+    pokemonNames.forEach((pokemon) => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`, { mode: "cors" })
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (response) {
+          const {
+            cries: { legacy },
+            id,
+            species: { name },
+            sprites: { front_default },
+          } = response;
+          setPokemonInfos((pokemonList) => [
+            ...pokemonList,
+            { legacy, id, name, front_default },
+          ]);
+        });
+    });
+  }, []);
+
+  return <div className="grid-container">{pokemons}</div>;
 }
 
 export { Grid };
