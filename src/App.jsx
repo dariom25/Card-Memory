8812c29670faa6c306d94bff763f4b9c7b7dba62
dariom25 <dario@maciey.de>
2024-03-26 15:42:51 +0100

    Add func to handle score related updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
 import { Grid } from "./Grid/grid";
 import { Scoreboard } from "./Scoreboard/scoreboard";
 
 export default function App() {
-  return (
+  const [score, setScore] = useState(0)
+  const [highscore, setHighscore] = useState(0)
+
+  const updateScore = () => {
+    setScore(score + 1)
+  }
+
+  const resetScore = () => {
+    setScore(prevScore => prevScore = 0)
+  }
+
+  const updateHighscore = () => {
+    setHighscore(prevHighscore => prevHighscore = score)
+  }
+  
+    return (
     <>
-      <Scoreboard />
-      <Grid />
+      <Scoreboard score={score} highscore={highscore}/>
+      <Grid scoreHandler={updateScore} scoreResetHandler={resetScore} highscoreHandler={updateHighscore}/>
     </>
   );
 }
