74026cecb6f37b4d4113f98c3d72a025c8e2905d
dariom25 <dario@maciey.de>
2024-03-26 15:43:54 +0100

    Handle clicking of card

--- src/Card/card.jsx
+++ src/Card/card.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 import "./card.css";
 
-function Card({ name, imageLink, scoreHandler, shuffleHandler }) {
+function Card({ name, imageLink, scoreHandler, shuffleHandler, scoreResetHandler, highscoreHandler }) {
   const [wasClicked, setWasClicked] = useState(false);
   const firstLetterUpperCase = (string) => {
     return string[0].toUpperCase() + string.slice(1);
   };
+
+  const handleScoreFunctions = () => {
+    !wasClicked ? handleClick() : handleAlreadyClickedCard()
+  }
+
+  const handleAlreadyClickedCard = () => {
+    highscoreHandler();
+    scoreResetHandler();
+    shuffleHandler();
+  }
+
   const handleClick = () => {
     setWasClicked(!wasClicked);
     scoreHandler();
     shuffleHandler();
   };
   return (
-    <div className="card-container" onClick={handleClick}>
+    <div className="card-container" onClick={handleScoreFunctions}>
       <div className="picture-container">
         <img src={imageLink} alt={firstLetterUpperCase(name)} />
       </div>
