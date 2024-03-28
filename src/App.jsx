a518d9e853534edfc802b0117ed01fbb2e79d006
dariom25 <dario@maciey.de>
2024-03-28 13:44:56 +0100

    Load Title component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Grid } from "./Grid/grid";
 import { Scoreboard } from "./Scoreboard/scoreboard";
+import { Title } from "./Title/title";
 
 export default function App() {
   const [score, setScore] = useState(0)
@@ -20,6 +21,7 @@ export default function App() {
   
     return (
     <>
+      <Title></Title>
       <Scoreboard score={score} highscore={highscore}/>
       <Grid scoreHandler={updateScore} scoreResetHandler={resetScore} highscoreHandler={updateHighscore}/>
     </>
