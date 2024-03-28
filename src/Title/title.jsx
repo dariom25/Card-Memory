9026c710dcf39cdf99b6bfd75d6251f94172cc53
dariom25 <dario@maciey.de>
2024-03-28 13:43:59 +0100

    Create Title component

---
+++ src/Title/title.jsx
@@ -0,0 +1,12 @@
+import "./title.css";
+
+function Title() {
+  return (
+    <div className="title">
+      <h1>Creepy Pokémon Memory</h1>
+      <p className="description">Memorize which Pokémon you already clicked!</p>
+    </div>
+  );
+}
+
+export { Title };
