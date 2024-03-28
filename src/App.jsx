a7d76f483795a84e2e6e7b03ed874ea373e63e17
dariom25 <dario@maciey.de>
2024-03-28 13:45:23 +0100

    Make code prettier

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,32 @@ import { Scoreboard } from "./Scoreboard/scoreboard";
 import { Title } from "./Title/title";
 
 export default function App() {
-  const [score, setScore] = useState(0)
-  const [highscore, setHighscore] = useState(0)
+  const [score, setScore] = useState(0);
+  const [highscore, setHighscore] = useState(0);
 
   const updateScore = () => {
-    setScore(score + 1)
-  }
+    setScore(score + 1);
+  };
 
   const resetScore = () => {
-    setScore(prevScore => prevScore = 0)
-  }
+    setScore((prevScore) => (prevScore = 0));
+  };
 
   const updateHighscore = () => {
-    if (highscore < score) {setHighscore(prevHighscore => prevHighscore = score)}
-  }
-  
-    return (
+    if (highscore < score) {
+      setHighscore((prevHighscore) => (prevHighscore = score));
+    }
+  };
+
+  return (
     <>
       <Title></Title>
-      <Scoreboard score={score} highscore={highscore}/>
-      <Grid scoreHandler={updateScore} scoreResetHandler={resetScore} highscoreHandler={updateHighscore}/>
+      <Scoreboard score={score} highscore={highscore} />
+      <Grid
+        scoreHandler={updateScore}
+        scoreResetHandler={resetScore}
+        highscoreHandler={updateHighscore}
+      />
     </>
   );
 }
