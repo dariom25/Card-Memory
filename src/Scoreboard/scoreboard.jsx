d7e9e8b39c58f1177e1d32bced3f50e4b9a516cb
dariom25 <dario@maciey.de>
2024-03-28 14:53:25 +0100

    Change p to h2

--- src/Scoreboard/scoreboard.jsx
+++ src/Scoreboard/scoreboard.jsx
@@ -1,8 +1,8 @@
 function Scoreboard({ score, highscore }) {
   return (
     <div className="scoreboard-container">
-      <p className="score">Current score: {score}</p>
-      <p className="highscore">Highscore: {highscore}</p>
+      <h2 className="score">Current score: {score}</h2>
+      <h2 className="highscore">Highscore: {highscore}</h2>
     </div>
   );
 }
